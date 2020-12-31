44e25acf54c939d4c8adf4ef73a9fa192a7209d3
dmachard <d.machard@gmail.com>
2020-12-31 15:32:03 +0100

    fix error in testunit decode

--- tests/test_decode.py
+++ tests/test_decode.py
@@ -9,7 +9,7 @@ class TestDecode(unittest.TestCase):
         data = b'\x00\x00\x00\x04\x00\x00\x00\x01\x00\x00\x00\x16protobuf:dnstap.Dnstap'
         f.append(data=data)
         ctrl, ct, payload  = f.decode()
-        self.assertEqual( len(ctrl), fstrm.FSTRM_CONTROL_READY )
+        self.assertEqual( ctrl, fstrm.FSTRM_CONTROL_READY )
         
     def test_decode_control_start(self):
         """decode control start"""   
@@ -18,7 +18,7 @@ class TestDecode(unittest.TestCase):
         data = b'\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x16protobuf:dnstap.Dnstap'
         f.append(data=data)
         ctrl, ct, payload  = f.decode()
-        self.assertEqual( len(ctrl), fstrm.FSTRM_CONTROL_START )
+        self.assertEqual( ctrl, fstrm.FSTRM_CONTROL_START )
         
     def test_decode_data_frame(self):
         """decode data frame"""   
@@ -27,4 +27,4 @@ class TestDecode(unittest.TestCase):
         data = b'\n\x08dnsdist1\x12\rdnsdist 1.5.0ru\x08\x05\x10\x02\x18\x01"\x10\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01*\x10\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x010\xc6\xac\x0385@\x9c\xbd\xb6\xff\x05M\xcb\xabL\x06R8~8\x01 \x00\x01\x00\x00\x00\x00\x00\x01\x03www\x07netflix\x03com\x00\x00\x01\x00\x01\x00\x00)\x10\x00\x00\x00\x00\x00\x00\x0c\x00\n\x00\x08a\xb7\xeb\x88\xc1|M\xf3x\x01'
         f.append(data=data)
         ctrl, ct, payload  = f.decode()
-        self.assertEqual( len(ctrl), fstrm.FSTRM_DATA_FRAME )
+        self.assertEqual( ctrl, fstrm.FSTRM_DATA_FRAME )
