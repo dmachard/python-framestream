4481ab247eabdc803d73d2925dde727044c9ae18
dmachard <d.machard@gmail.com>
2020-12-31 16:09:50 +0100

    fix testunit

--- tests/test_codec.py
+++ tests/test_codec.py
@@ -9,7 +9,9 @@ class TestCodec(unittest.TestCase):
         data_bin = f.encode(ctrl=fstrm.FSTRM_CONTROL_READY, ct=[b"protobuf:dnstap.Dnstap"])
         f.append(data=data_bin)
         
-        ft, ct, payload  = f.decode()
+        ft = -1
+        if f.process():
+            ft, ct, payload  = f.decode()
         self.assertEqual( ft, fstrm.FSTRM_CONTROL_READY )
         
     def test_control_accept(self):
@@ -46,7 +48,7 @@ class TestCodec(unittest.TestCase):
         ft = -1
         if f.process():
             ft, ct, payload  = f.decode()
-        self.assertEqual( ft, fstrm.FSTRM_CONTROL_STOP )
+        self.assertEqual( ft, fstrm.FSTRM_CONTROL_START )
         
     def test_decode_control_stop(self):
         """decode control stop"""   
